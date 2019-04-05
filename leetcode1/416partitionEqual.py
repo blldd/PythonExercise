d02a2d1153f4032b6b2b09ec13a3c4bac137e9f6
dedong.li <18201788952@163.com>
2019-04-05 21:03:32 +0800

    划分子数组，使得差最小问题，dp

--- leetcode1/416partitionEqual.py
+++ leetcode1/416partitionEqual.py
@@ -4,7 +4,8 @@ from math import ceil
 class Solution(object):
     def canPartition(self, nums):
         """
-        划分成两个相等的子数组
+        划分成两个和相等的子数组
+        转化成 给定数组 和 target 求是否存在子数组能组合成target
         :type nums: List[int]
         :rtype: bool
         """
@@ -23,61 +24,35 @@ class Solution(object):
 
         return dp[half_sum] == 1
 
-    def partition1(self, nums):
-        """
-        和差值最小子数组划分 问题
-        有 bug
-
-        :param nums:
-        :return:
-        """
-        length = len(nums)
-        nums = sorted(nums, reverse=True)
-        total = sum(nums)
-        target = ceil(total / 2)
-
-        a_list = []
-        b_list = []
-
-        s = 0
-        for i in range(length):
-            try:
-                if s + nums[i] <= target:
-                    s += nums[i]
-                    a_list.append(nums[i])
-                    # nums.pop(i)
-                    if abs(s - target) < a_list[0]:
-                        break
-                else:
-                    b_list.append(nums[i])
-            except Exception as e:
-                print(i, e)
-        return a_list, b_list
-
-    def partition(self, nums):
+    def partition_dp(self, nums):
         """
         动态规划
-        状态方程：f[i][j]=Max(f[i-1][j-array[i]]+array[i],f[i-1][j])
+        状态方程：f[j]=Max(f[j-array[i]]+array[i],f[j])
         :param nums:
         :return:
         """
-        length = len(nums)
-        nums = sorted(nums, reverse=True)
         total = sum(nums)
+        current_sum = [0 for i in range(total // 2 + 1)]
+        state = [[0 for j in range(total // 2 + 1)] for i in range(len(nums))]
 
-        dp = [[0 for j in range(total // 2 + 1)] for i in range(length + 1)]
-
-        for i in range(length):
-            for j in range(1, total // 2 + 1):
-                dp[i + 1][j] = dp[i][j]
-                if nums[i] <= j and dp[i][j - nums[i]] + nums[i] > dp[i][j]:
-                    dp[i + 1][j] = dp[i][j - nums[i]] + nums[i]
-
-        for i in dp:
+        for i in range(len(nums)):
+            for j in range(total // 2, nums[i] - 1, -1):
+                if current_sum[j] < current_sum[j - nums[i]] + nums[i]:
+                    current_sum[j] = current_sum[j - nums[i]] + nums[i]
+                    state[i][j] = 1
+        for i in state:
             print(i)
+        print(current_sum)
+        res = []
+        j = total // 2
+        for i in range(len(nums) - 1, -1, -1):
+            if state[i][j]:
+                res.append(nums[i])
+                j -= nums[i]
+        return res
 
 
 if __name__ == '__main__':
-    nums = [2, 2, 3, 5, 5, 9]
-    print(Solution().canPartition(nums))
-    print(Solution().partition(nums))
+    nums = [5, 2, 3, 5, 7]
+    # print(Solution().canPartition(nums))
+    print(sum(Solution().partition_dp(nums)))
