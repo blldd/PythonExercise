dfb972708c97306af6359a341405b7db1bf6e819
dedong.li <18201788952@163.com>
2019-08-08 22:35:49 +0800

    min path dp

--- leetcode1/64minPathSum.py
+++ leetcode1/64minPathSum.py
@@ -4,6 +4,7 @@
 @Author  : ddlee
 @File    : 64minPathSum.py
 """
+import sys
 
 """
 64. 最小路径和
@@ -22,17 +23,113 @@
 
 
 class Solution:
-    def minPathSum(self, grid):
-        def dfs(grid, i, j, res):
-            return res
+    """
+    时间复杂度 ：O(2^{m+n})。每次移动最多可以有两种选择。
+    空间复杂度 ：O(m+n)。递归的深度是 m+n
+    return grid[i][j] + min(dfs(grid, i + 1, j), dfs(grid, i, j + 1));
+    """
 
-        res = 0
-        res = dfs(grid, 0, 0, res)
-        return
+    def minPathSum_naive(self, grid):
+        m = len(grid)
+        if m < 1:
+            return 0
+        n = len(grid[0])
+        if n < 1:
+            return 0
+
+        def dfs(grid, i, j):
+            if i == m or j == n:
+                return sys.maxsize
+            if i == m - 1 and j == n - 1:
+                return grid[i][j]
+            return grid[i][j] + min(dfs(grid, i + 1, j), dfs(grid, i, j + 1));
+
+        res = dfs(grid, 0, 0)
+        return res
+
+    """
+    时间复杂度 ：O(mn)。遍历整个矩阵恰好一次。
+    空间复杂度 ：O(mn)。额外的一个同大小矩阵。
+    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i - 1][j - 1]
+    """
+
+    def minPathSum_dp_2d(self, grid):
+        m = len(grid)
+        if m < 1:
+            return 0
+        n = len(grid[0])
+        if n < 1:
+            return 0
+
+        dp = [[sys.maxsize for _ in range(n + 1)] for _ in range(m + 1)]
+        dp[0][1] = 0
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i - 1][j - 1]
+
+        for i in dp:
+            print(i)
+        return dp[-1][-1]
+
+    """
+    时间复杂度 ：O(mn)。遍历整个矩阵恰好一次。
+    空间复杂度 ：O(n)。
+    dp[j] = min(dp[j], dp[j - 1]) + grid[i - 1][j - 1]
+    """
+
+    def minPathSum_dp_1d(self, grid):
+        m = len(grid)
+        if m < 1:
+            return 0
+        n = len(grid[0])
+        if n < 1:
+            return 0
+
+        dp = [sys.maxsize for _ in range(n + 1)]
+        dp[1] = 0
+
+        for i in range(1, m + 1):
+            print(dp)
+            for j in range(1, n + 1):
+                dp[j] = min(dp[j], dp[j - 1]) + grid[i - 1][j - 1]
+
+        print(dp)
+        return dp[-1]
+
+    """
+    时间复杂度 ：O(mn)。遍历整个矩阵恰好一次。
+    空间复杂度 ：O(1)。原来矩阵上修改
+    grid(i,j)=grid(i,j)+min(grid(i+1,j),grid(i,j+1))
+    """
+
+    def minPathSum_dp_1(self, grid):
+        m = len(grid)
+        if m < 1:
+            return 0
+        n = len(grid[0])
+        if n < 1:
+            return 0
+
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    continue
+                elif i == 0 and j != 0:
+                    grid[i][j] = grid[i][j - 1] + grid[i][j]
+                elif i != 0 and j == 0:
+                    grid[i][j] = grid[i - 1][j] + grid[i][j]
+                else:
+                    grid[i][j] = min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j]
+
+        return grid[-1][-1]
 
 
 if __name__ == '__main__':
     grid = [[1, 3, 1],
             [1, 5, 1],
             [4, 2, 1]]
-    print(Solution().minPathSum(grid))
+    print(Solution().minPathSum_naive(grid))
+    print(Solution().minPathSum_dp_2d(grid))
+    print(Solution().minPathSum_dp_1d(grid))
+    print(Solution().minPathSum_dp_1(grid))
