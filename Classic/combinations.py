e5e698028b6657f6197681abf748615df24bd62c
dedong.li <18201788952@163.com>
2019-08-22 21:55:33 +0800

    combine backtrack

--- Classic/combinations.py
+++ Classic/combinations.py
@@ -6,6 +6,7 @@
 """
 import itertools
 
+
 # itertools.combinations()
 def combinations(iterable, r):
     pool = tuple(iterable)
@@ -28,8 +29,28 @@ def combinations(iterable, r):
         yield tuple(pool[i] for i in indices)
 
 
+def combine(n, k):
+    def backtrack(first=1, curr=[]):
+        # if the combination is done
+        if len(curr) == k:
+            res.append(curr.copy())
+        for i in range(first, n + 1):
+            # add i into the current combination
+            curr.append(i)
+            # use next integers to complete the combination
+            backtrack(i + 1, curr)
+            # backtrack
+            curr.pop()
+
+    res = []
+    backtrack()
+    return res
+
+
 if __name__ == '__main__':
     # combinations('ABCD', 2) --> AB AC AD BC BD CD
     # combinations(range(4), 3) --> 012 013 023 123
     for i in combinations(range(1, 5), 2):
         print(i)
+
+    print(combine(n=4, k=2))
