698e663db6d2cd93541492bdff86cf6997c24848
dedong.li <18201788952@163.com>
2019-09-05 21:50:43 +0800

    josephus

--- Classic/josephus.py
+++ Classic/josephus.py
@@ -6,7 +6,7 @@
 """
 
 
-def josephus(n, m):
+def josephus_formula(n, m):
     """
     :param n: 总人数
     :param m: 每数到m去除
@@ -16,7 +16,51 @@ def josephus(n, m):
     if n == 1:
         return 0
     else:
-        return (josephus(n - 1, m) + m) % n
+        return (josephus_formula(n - 1, m) + m) % n
+
+
+def josephus_mimic(n, k):
+    link = list(range(1, n + 1))
+    ind = 0
+
+    for loop_i in range(n - 1):
+        ind = (ind + k) % len(link)
+        ind -= 1
+
+        print('Kill:', link[ind])
+        del link[ind]
+        if ind == -1:  # the last element of link
+            ind = 0
+
+    print('survice :', link[0])
+
+
+def josephus(n, m):
+    """
+
+    Args:
+        n: n people
+        m: count m del
+
+    Returns: last survive
+
+    """
+    people = list(range(1, n + 1))
+    idx = 0
+
+    last = 0
+    for i in range(n):
+        cnt = 0
+        while cnt < m:
+            if people[idx] != -1:
+                cnt += 1
+            if cnt == m:
+                # print(people[idx])
+                last = people[idx]
+                people[idx] = -1
+            idx = (idx + 1) % n
+
+    return last
 
 
 if __name__ == '__main__':
