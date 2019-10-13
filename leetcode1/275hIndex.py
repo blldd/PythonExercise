87e53db49aa6c3aadfee5a98bc65fc2b152512ef
dedong.li <18201788952@163.com>
2019-10-13 08:41:33 +0800

    hindex

---
+++ leetcode1/275hIndex.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2019/10/10 7:46 PM
+@Author  : ddlee
+@File    : 275hIndex.py
+"""
+"""
+275. H指数 II
+
+给定一位研究者论文被引用次数的数组（被引用次数是非负整数），数组已经按照升序排列。
+编写一个方法，计算出研究者的 h 指数。
+
+h 指数的定义: “h 代表“高引用次数”（high citations），
+一名科研人员的 h 指数是指他（她）的 （N 篇论文中）至多有 h 篇论文分别被引用了至少 h 次。
+（其余的 N - h 篇论文每篇被引用次数不多于 h 次。）"
+
+示例:
+
+输入: citations = [0,1,3,5,6]
+输出: 3 
+解释: 给定数组表示研究者总共有 5 篇论文，每篇论文相应的被引用了 0, 1, 3, 5, 6 次。
+     由于研究者有 3 篇论文每篇至少被引用了 3 次，其余两篇论文每篇被引用不多于 3 次，所以她的 h 指数是 3。
+
+说明:
+如果 h 有多有种可能的值 ，h 指数是其中最大的那个。
+
+进阶：
+
+这是 H指数 的延伸题目，本题中的 citations 数组是保证有序的。
+你可以优化你的算法到对数时间复杂度吗？
+在真实的面试中遇到过这道题？
+"""
+
+
+class Solution:
+
+    def hIndex_sort(self, citations) -> int:
+        l = len(citations)
+        if l < 1:
+            return 0
+
+        citations = citations[::-1]
+        for i in range(l):
+            if citations[i] <= i:
+                return i
+        return l
+
+    # one by one
+    def hIndex(self, citations) -> int:
+        l = len(citations)
+        if l < 1:
+            return 0
+
+        for i in range(l - 1, -1, -1):
+            if citations[i] < l - i:
+                return l - i - 1
+        return l
+
+    # binary search
+    def hIndex_binary(self, citations) -> int:
+        l = len(citations)
+        if l < 1:
+            return 0
+
+        for i in range(l - 1, -1, -1):
+            if citations[i] < l - i:
+                return l - i - 1
+
+        low = 0
+        high = l
+        while low < high:
+            mid = (low + high) >> 1
+            if citations[mid] < l - mid:
+                return l - mid - 1
+            else:
+                high = mid
+        return l
+
+
+if __name__ == '__main__':
+    citations = [0, 1, 3, 5, 6]
+    # citations = [0]
+    print(Solution().hIndex_binary(citations))
